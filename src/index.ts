a8bb7430050d0369dacce2f5fbbd7d9c64dd48ad
Fernando.LC <fer.lc.developer@gmail.com>
2023-03-27 01:49:20 -0600

    feat(express): Config Express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const messagess = 'Hello, TypeScript!'
-console.log(messagess)
+import express from 'express'
+
+const { PORT } = process.env
+const NEW_PORT = PORT || 3002
+const APP = express()
+
+APP.use(express.json())
+
+APP.get('*', (request, response) => {
+  response.send('🔥 4 0 4 🔥')
+})
+
+APP.listen(NEW_PORT, async () => {
+  console.log(`Server successfull: http://localhost:${NEW_PORT}`)
+})
